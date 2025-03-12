33f84f4c07de4cf77fd2db572470fc29e2b550cf
KHuang00 <Kathiehuang0@gmail.com>
2025-03-12 18:21:10 -0400

    add assignments 4

--- kambaz-react-web-app/src/Kambaz/Courses/index.tsx
+++ kambaz-react-web-app/src/Kambaz/Courses/index.tsx
@@ -100,190 +100,127 @@
 // }
 //
 
-// import { useEffect, useState } from "react";
-// import { FaChevronDown, FaChevronUp, FaAlignJustify } from "react-icons/fa";
-// import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
-// import { useSelector } from "react-redux"; // Import Redux state
-// import { courses as allCourses, enrollments } from "../Database/index.tsx";
-// import CoursesNavigation from "./Navigations";
-// import Modules from "./Modules";
-// import Home from "./Home";
-// import Assignments from "./Assignments";
-// import AssignmentEditor from "./Assignments/Editor";
-// import PeopleTable from "./people/Table.tsx";
-// import "./index.css";
-//
-// export default function Courses() {
-//     const { courseId } = useParams();
-//     const { pathname } = useLocation();
-//     const [showNav, setShowNav] = useState(false);
-//     const [section, setSection] = useState("Home");
-//     const [userCourses, setUserCourses] = useState([]);
-//
-//     // @ts-ignore
-//     const currentUser = useSelector((state) => state.account.currentUser);
-//     console.log("Current User:", currentUser);
-//
-//
-//     useEffect(() => {
-//         if (currentUser) {
-//             // Get enrollments for the current user
-//             const userEnrollments = enrollments.filter(e => e.user === currentUser._id);
-//
-//             // Get courses based on enrollments
-//             const filteredCourses = allCourses.filter(course =>
-//                 userEnrollments.some(enrollment => enrollment.course === course._id)
-//             );
-//
-//             // @ts-ignore
-//             setUserCourses(filteredCourses);
-//         }
-//     }, [currentUser]);
-//
-//     useEffect(() => {
-//         const pathParts = pathname.split("/");
-//         setSection(pathParts.length > 4 ? pathParts[4] : "Home");
-//     }, [pathname]);
-//
-//     // @ts-ignore
-//     const course = userCourses.find((course) => course._id === courseId);
-//
-//     const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
-//
-//
-//     return (
-//         <div id="wd-courses" className="container-fluid">
-//             <h2 className="text-danger d-flex align-items-center justify-content-between">
-//                 <span>
-//                     <FaAlignJustify className="me-3 fs-4 mb-1" />
-//                       {/* @ts-ignore*/}
-//                     {course ? `${course.name} > ${section}` : `Course Not Found`}
-//                 </span>
-//                 <span
-//                     className="fs-4 mb-1 d-md-none"
-//                     onClick={() => setShowNav((prev) => !prev)}
-//                     style={{ cursor: "pointer" }}
-//                 >
-//                     {showNav ? <FaChevronUp /> : <FaChevronDown />}
-//                 </span>
-//             </h2>
-//
-//             {showNav && (
-//                 <div
-//                     className="bg-white p-3 shadow-sm"
-//                     style={{
-//                         position: "absolute",
-//                         top: "60px",
-//                         left: 0,
-//                         width: "100%",
-//                         zIndex: 1000,
-//                         borderBottom: "1px solid #ddd",
-//                     }}
-//                 >
-//                     <ul className="list-unstyled">
-//                         {links.map(
-//                             (item) => (
-//                                 <li key={item} className="p-2 text-danger" onClick={() => setShowNav(false)}>
-//                                     {item}
-//                                 </li>
-//                             )
-//                         )}
-//                     </ul>
-//                 </div>
-//             )}
-//
-//             <div className="row">
-//                 <div className="col-md-3 d-none d-md-block">
-//                     <CoursesNavigation />
-//                 </div>
-//
-//                 <div className="col-md-9">
-//                     {course ? (
-//                         <Routes>
-//                             <Route path="/" element={<Navigate to="Home" />} />
-//                             <Route path="Home" element={<Home />} />
-//                             <Route path="Modules" element={<Modules />} />
-//                             <Route path="Piazza" element={<h2>Piazza</h2>} />
-//                             <Route path="Zoom" element={<h2>Zoom</h2>} />
-//                             <Route path="Assignments" element={<Assignments />} />
-//                             <Route path="Assignments/:assignmentId" element={<AssignmentEditor />} />
-//                             <Route path="Quizzes" element={<h2>Quizzes</h2>} />
-//                             <Route path="Exams" element={<h2>Exams</h2>} />
-//                             <Route path="Projects" element={<h2>Projects</h2>} />
-//                             <Route path="Projects/:pid" element={<AssignmentEditor />} />
-//                             <Route path="Grades" element={<h2>Grades</h2>} />
-//                             <Route path="People" element={<PeopleTable />} />
-//                         </Routes>
-//                     ) : (
-//                         <p className="text-danger">You are not enrolled in this course.</p>
-//                     )}
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-//
-
-
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { FaChevronDown, FaChevronUp, FaAlignJustify } from "react-icons/fa";
+import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
+import { useSelector } from "react-redux"; // Import Redux state
 import { courses as allCourses, enrollments } from "../Database/index.tsx";
+import CoursesNavigation from "./Navigations";
+import Modules from "./Modules";
+import Home from "./Home";
+import Assignments from "./Assignments";
+import AssignmentEditor from "./Assignments/Editor";
+import PeopleTable from "./people/Table.tsx";
+import "./index.css";
 
 export default function Courses() {
-    // @ts-ignore
-    const currentUser = useSelector((state) => state.account.currentUser);
+    const { courseId } = useParams();
+    const { pathname } = useLocation();
+    const [showNav, setShowNav] = useState(false);
+    const [section, setSection] = useState("Home");
     const [userCourses, setUserCourses] = useState([]);
 
-    console.log("Current User from Redux:", currentUser);
+    // @ts-ignore
+    const currentUser = useSelector((state) => state.account.currentUser);
+    console.log("Current User:", currentUser);
+
 
     useEffect(() => {
-        if (!currentUser) {
-            console.log(" Waiting for Redux to load `currentUser`...");
-            return;
-        }
+        if (currentUser) {
+            // Get enrollments for the current user
+            const userEnrollments = enrollments.filter(e => e.user === currentUser._id);
 
-        console.log("🔎 Checking enrollments for user:", currentUser._id);
+            // Get courses based on enrollments
+            const filteredCourses = allCourses.filter(course =>
+                userEnrollments.some(enrollment => enrollment.course === course._id)
+            );
 
-        // Ensure enrollment user IDs match `currentUser._id`
-        const userEnrollments = enrollments.filter(e => String(e.user) === String(currentUser._id));
-        console.log("User Enrollments:", userEnrollments);
-
-        if (userEnrollments.length === 0) {
-            console.log(" No enrollments found for user:", currentUser._id);
-        }
-
-        // Ensure course IDs match enrollments
-        const filteredCourses = allCourses.filter(course =>
-            userEnrollments.some(enrollment => String(enrollment.course) === String(course._id))
-        );
-
-        console.log(" Filtered Courses:", filteredCourses);
-
-        // Set state only when courses are found
-        if (filteredCourses.length > 0) {
             // @ts-ignore
             setUserCourses(filteredCourses);
-        } else {
-            console.log(" No courses found for user.");
         }
     }, [currentUser]);
 
+    useEffect(() => {
+        const pathParts = pathname.split("/");
+        setSection(pathParts.length > 4 ? pathParts[4] : "Home");
+    }, [pathname]);
+
+    // @ts-ignore
+    const course = userCourses.find((course) => course._id === courseId);
+
+    const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
+
+
     return (
-        <div className="container">
-            <h3> Your Courses</h3>
-            {userCourses.length > 0 ? (
-                userCourses.map((course) => (
-                    // @ts-ignore
-                    <div key={course._id}>
-                        {/*// @ts-ignore*/}
-                        <h4>{course.title}</h4>
-                        {/*// @ts-ignore*/}
-                        <p>{course.description}</p>
-                    </div>
-                ))
-            ) : (
-                <p>️ No courses found.</p>
+        <div id="wd-courses" className="container-fluid">
+            <h2 className="text-danger d-flex align-items-center justify-content-between">
+                <span>
+                    <FaAlignJustify className="me-3 fs-4 mb-1" />
+                      {/* @ts-ignore*/}
+                    {course ? `${course.name} > ${section}` : `Course Not Found`}
+                </span>
+                <span
+                    className="fs-4 mb-1 d-md-none"
+                    onClick={() => setShowNav((prev) => !prev)}
+                    style={{ cursor: "pointer" }}
+                >
+                    {showNav ? <FaChevronUp /> : <FaChevronDown />}
+                </span>
+            </h2>
+
+            {showNav && (
+                <div
+                    className="bg-white p-3 shadow-sm"
+                    style={{
+                        position: "absolute",
+                        top: "60px",
+                        left: 0,
+                        width: "100%",
+                        zIndex: 1000,
+                        borderBottom: "1px solid #ddd",
+                    }}
+                >
+                    <ul className="list-unstyled">
+                        {links.map(
+                            (item) => (
+                                <li key={item} className="p-2 text-danger" onClick={() => setShowNav(false)}>
+                                    {item}
+                                </li>
+                            )
+                        )}
+                    </ul>
+                </div>
             )}
+
+            <div className="row">
+                <div className="col-md-3 d-none d-md-block">
+                    <CoursesNavigation />
+                </div>
+
+                <div className="col-md-9">
+                    {course ? (
+                        <Routes>
+                            <Route path="/" element={<Navigate to="Home" />} />
+                            <Route path="Home" element={<Home />} />
+                            <Route path="Modules" element={<Modules />} />
+                            <Route path="Piazza" element={<h2>Piazza</h2>} />
+                            <Route path="Zoom" element={<h2>Zoom</h2>} />
+                            <Route path="Assignments" element={<Assignments />} />
+                            <Route path="Assignments/:assignmentId" element={<AssignmentEditor />} />
+                            <Route path="Quizzes" element={<h2>Quizzes</h2>} />
+                            <Route path="Exams" element={<h2>Exams</h2>} />
+                            <Route path="Projects" element={<h2>Projects</h2>} />
+                            <Route path="Projects/:pid" element={<AssignmentEditor />} />
+                            <Route path="Grades" element={<h2>Grades</h2>} />
+                            <Route path="People" element={<PeopleTable />} />
+                        </Routes>
+                    ) : (
+                        <p className="text-danger">You are not enrolled in this course.</p>
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
+
+
