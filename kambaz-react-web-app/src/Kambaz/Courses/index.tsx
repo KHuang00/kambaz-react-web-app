7efcbed5c1e976b55cefc3c1546faa8b277e6fd9
KHuang00 <Kathiehuang0@gmail.com>
2025-03-12 18:33:31 -0400

    add assignments 4

--- kambaz-react-web-app/src/Kambaz/Courses/index.tsx
+++ kambaz-react-web-app/src/Kambaz/Courses/index.tsx
@@ -100,11 +100,137 @@
 // }
 //
 
+// import { useEffect, useState } from "react";
+// import { FaChevronDown, FaChevronUp, FaAlignJustify } from "react-icons/fa";
+// import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
+// import { useSelector } from "react-redux"; // Import Redux state
+// import { courses as allCourses, enrollments } from "../Database/index.tsx";
+// import CoursesNavigation from "./Navigations";
+// import Modules from "./Modules";
+// import Home from "./Home";
+// import Assignments from "./Assignments";
+// import AssignmentEditor from "./Assignments/Editor";
+// import PeopleTable from "./people/Table.tsx";
+// import "./index.css";
+//
+// export default function Courses() {
+//     const { courseId } = useParams();
+//     const { pathname } = useLocation();
+//     const [showNav, setShowNav] = useState(false);
+//     const [section, setSection] = useState("Home");
+//     const [userCourses, setUserCourses] = useState([]);
+//
+//     // @ts-ignore
+//     const currentUser = useSelector((state) => state.account.currentUser);
+//     console.log("Current User:", currentUser);
+//
+//
+//     useEffect(() => {
+//         if (currentUser) {
+//             // Get enrollments for the current user
+//             const userEnrollments = enrollments.filter(e => e.user === currentUser._id);
+//
+//             // Get courses based on enrollments
+//             const filteredCourses = allCourses.filter(course =>
+//                 userEnrollments.some(enrollment => enrollment.course === course._id)
+//             );
+//
+//             // @ts-ignore
+//             setUserCourses(filteredCourses);
+//         }
+//     }, [currentUser]);
+//
+//     useEffect(() => {
+//         const pathParts = pathname.split("/");
+//         setSection(pathParts.length > 4 ? pathParts[4] : "Home");
+//     }, [pathname]);
+//
+//     // @ts-ignore
+//     const course = userCourses.find((course) => course._id === courseId);
+//
+//     const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
+//
+//
+//     return (
+//         <div id="wd-courses" className="container-fluid">
+//             <h2 className="text-danger d-flex align-items-center justify-content-between">
+//                 <span>
+//                     <FaAlignJustify className="me-3 fs-4 mb-1" />
+//                       {/* @ts-ignore*/}
+//                     {course ? `${course.name} > ${section}` : `Course Not Found`}
+//                 </span>
+//                 <span
+//                     className="fs-4 mb-1 d-md-none"
+//                     onClick={() => setShowNav((prev) => !prev)}
+//                     style={{ cursor: "pointer" }}
+//                 >
+//                     {showNav ? <FaChevronUp /> : <FaChevronDown />}
+//                 </span>
+//             </h2>
+//
+//             {showNav && (
+//                 <div
+//                     className="bg-white p-3 shadow-sm"
+//                     style={{
+//                         position: "absolute",
+//                         top: "60px",
+//                         left: 0,
+//                         width: "100%",
+//                         zIndex: 1000,
+//                         borderBottom: "1px solid #ddd",
+//                     }}
+//                 >
+//                     <ul className="list-unstyled">
+//                         {links.map(
+//                             (item) => (
+//                                 <li key={item} className="p-2 text-danger" onClick={() => setShowNav(false)}>
+//                                     {item}
+//                                 </li>
+//                             )
+//                         )}
+//                     </ul>
+//                 </div>
+//             )}
+//
+//             <div className="row">
+//                 <div className="col-md-3 d-none d-md-block">
+//                     <CoursesNavigation />
+//                 </div>
+//
+//                 <div className="col-md-9">
+//                     {course ? (
+//                         <Routes>
+//                             <Route path="/" element={<Navigate to="Home" />} />
+//                             <Route path="Home" element={<Home />} />
+//                             <Route path="Modules" element={<Modules />} />
+//                             <Route path="Piazza" element={<h2>Piazza</h2>} />
+//                             <Route path="Zoom" element={<h2>Zoom</h2>} />
+//                             <Route path="Assignments" element={<Assignments />} />
+//                             <Route path="Assignments/:assignmentId" element={<AssignmentEditor />} />
+//                             <Route path="Quizzes" element={<h2>Quizzes</h2>} />
+//                             <Route path="Exams" element={<h2>Exams</h2>} />
+//                             <Route path="Projects" element={<h2>Projects</h2>} />
+//                             <Route path="Projects/:pid" element={<AssignmentEditor />} />
+//                             <Route path="Grades" element={<h2>Grades</h2>} />
+//                             <Route path="People" element={<PeopleTable />} />
+//                         </Routes>
+//                     ) : (
+//                         <p className="text-danger">You are not enrolled in this course.</p>
+//                     )}
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+//
+
+
+
 import { useEffect, useState } from "react";
 import { FaChevronDown, FaChevronUp, FaAlignJustify } from "react-icons/fa";
 import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
 import { useSelector } from "react-redux"; // Import Redux state
-import { courses as allCourses, enrollments } from "../Database/index.tsx";
+import { enrollments } from "../Database/index.tsx"; // Import only enrollments
 import CoursesNavigation from "./Navigations";
 import Modules from "./Modules";
 import Home from "./Home";
@@ -113,7 +239,8 @@ import AssignmentEditor from "./Assignments/Editor";
 import PeopleTable from "./people/Table.tsx";
 import "./index.css";
 
-export default function Courses() {
+// @ts-ignore
+export default function Courses({ courses }) {
     const { courseId } = useParams();
     const { pathname } = useLocation();
     const [showNav, setShowNav] = useState(false);
@@ -124,21 +251,23 @@ export default function Courses() {
     const currentUser = useSelector((state) => state.account.currentUser);
     console.log("Current User:", currentUser);
 
-
     useEffect(() => {
         if (currentUser) {
-            // Get enrollments for the current user
-            const userEnrollments = enrollments.filter(e => e.user === currentUser._id);
+            console.log("Checking enrollments for user:", currentUser._id);
+            // @ts-ignore
+            const userEnrollments = enrollments.filter(e => String(e.user) === String(currentUser._id));
+            console.log("User Enrollments:", userEnrollments);
 
-            // Get courses based on enrollments
-            const filteredCourses = allCourses.filter(course =>
-                userEnrollments.some(enrollment => enrollment.course === course._id)
+            // @ts-ignore
+            const filteredCourses = courses.filter(course =>
+                userEnrollments.some(enrollment => String(enrollment.course) === String(course._id))
             );
 
+            console.log("Filtered Courses:", filteredCourses);
             // @ts-ignore
             setUserCourses(filteredCourses);
         }
-    }, [currentUser]);
+    }, [currentUser, courses]);
 
     useEffect(() => {
         const pathParts = pathname.split("/");
@@ -146,17 +275,16 @@ export default function Courses() {
     }, [pathname]);
 
     // @ts-ignore
-    const course = userCourses.find((course) => course._id === courseId);
+    const course = userCourses.find((course) => String(course._id) === String(courseId));
 
     const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
-
 
     return (
         <div id="wd-courses" className="container-fluid">
             <h2 className="text-danger d-flex align-items-center justify-content-between">
                 <span>
                     <FaAlignJustify className="me-3 fs-4 mb-1" />
-                      {/* @ts-ignore*/}
+                    {/* @ts-ignore */}
                     {course ? `${course.name} > ${section}` : `Course Not Found`}
                 </span>
                 <span
@@ -222,5 +350,3 @@ export default function Courses() {
         </div>
     );
 }
-
-
--- kambaz-react-web-app/src/Kambaz/index.tsx
+++ kambaz-react-web-app/src/Kambaz/index.tsx
@@ -1,3 +1,72 @@
+// import { Navigate, Route, Routes } from "react-router-dom";
+// import Account from "./Account";
+// import Dashboard from "./Dashboard";
+// import KambazNavigation from "./Navigation";
+// import Courses from "./Courses";
+// import "./index.css";
+// import * as db from "./Database";
+// import { useState } from "react";
+// import { v4 as uuidv4 } from "uuid";
+// import ProtectedRoute from "./Account/ProtectedRoute.tsx";
+//
+// export default function Kambaz() {
+//
+//     const [courses, setCourses] = useState<any[]>(db.courses);
+//     const [course, setCourse] = useState<any>({
+//         _id: "1234", name: "New Course", number: "New Number",
+//         startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
+//     });
+//     const addNewCourse = () => {
+//         setCourses([...courses, { ...course, _id: uuidv4() }]);
+//     };
+//     const deleteCourse = (courseId: any) => {
+//         setCourses(courses.filter((course) => course._id !== courseId));
+//     };
+//     const updateCourse = () => {
+//         setCourses(
+//             courses.map((c) => {
+//                 if (c._id === course._id) {
+//                     return { ...course, _id: c._id };
+//                 } else {
+//                     return c;
+//                 }
+//             })
+//         );
+//     };
+//
+//     return (
+//         <div id="wd-kambaz">
+//             {/*<table>*/}
+//             {/*    <tr>*/}
+//             {/*        <td valign="top">*/}
+//                         <KambazNavigation />
+//                     {/*</td>*/}
+//                     <div className="wd-main-content-offset p-3">
+//                         <Routes>
+//                             <Route path="/" element={<Navigate to="Account" />} />
+//                             <Route path="/Account/*" element={<Account />} />
+//                             <Route path="/Dashboard" element={
+//                                <ProtectedRoute>
+//                                 <Dashboard
+//                                     //@ts-ignore
+//                                     courses={courses}
+//                                     course={course}
+//                                     setCourse={setCourse}
+//                                     addNewCourse={addNewCourse}
+//                                     deleteCourse={deleteCourse}
+//                                     updateCourse={updateCourse}/></ProtectedRoute>
+//                             } />
+//                             {/*<Route path="/Courses/:cid/*" element={<Courses />} />*/}
+//                             {/*@ts-ignore*/}
+//                             <Route path="/Courses/:courseId/*"  element={<ProtectedRoute><Courses courses={courses} /></ProtectedRoute> } />
+//                             <Route path="/Calendar" element={<h1>Calendar</h1>} />
+//                             <Route path="/Inbox" element={<h1>Inbox</h1>} />
+//                         </Routes>
+//                     </div>
+//             {/*    </tr>*/}
+//             {/*</table>*/}
+//         </div>
+//     );}
 import { Navigate, Route, Routes } from "react-router-dom";
 import Account from "./Account";
 import Dashboard from "./Dashboard";
@@ -10,18 +79,25 @@ import { v4 as uuidv4 } from "uuid";
 import ProtectedRoute from "./Account/ProtectedRoute.tsx";
 
 export default function Kambaz() {
-
+    // @ts-ignore
     const [courses, setCourses] = useState<any[]>(db.courses);
+    // @ts-ignore
     const [course, setCourse] = useState<any>({
         _id: "1234", name: "New Course", number: "New Number",
         startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
     });
+
+    // @ts-ignore
     const addNewCourse = () => {
         setCourses([...courses, { ...course, _id: uuidv4() }]);
     };
+
+    // @ts-ignore
     const deleteCourse = (courseId: any) => {
         setCourses(courses.filter((course) => course._id !== courseId));
     };
+
+    // @ts-ignore
     const updateCourse = () => {
         setCourses(
             courses.map((c) => {
@@ -36,33 +112,40 @@ export default function Kambaz() {
 
     return (
         <div id="wd-kambaz">
-            {/*<table>*/}
-            {/*    <tr>*/}
-            {/*        <td valign="top">*/}
-                        <KambazNavigation />
-                    {/*</td>*/}
-                    <div className="wd-main-content-offset p-3">
-                        <Routes>
-                            <Route path="/" element={<Navigate to="Account" />} />
-                            <Route path="/Account/*" element={<Account />} />
-                            <Route path="/Dashboard" element={
-                               <ProtectedRoute>
-                                <Dashboard
-                                    //@ts-ignore
-                                    courses={courses}
-                                    course={course}
-                                    setCourse={setCourse}
-                                    addNewCourse={addNewCourse}
-                                    deleteCourse={deleteCourse}
-                                    updateCourse={updateCourse}/></ProtectedRoute>
-                            } />
-                            {/*<Route path="/Courses/:cid/*" element={<Courses />} />*/}
-                            <Route path="/Courses/:courseId/*"  element={<ProtectedRoute><Courses courses={courses} /></ProtectedRoute> } />
-                            <Route path="/Calendar" element={<h1>Calendar</h1>} />
-                            <Route path="/Inbox" element={<h1>Inbox</h1>} />
-                        </Routes>
-                    </div>
-            {/*    </tr>*/}
-            {/*</table>*/}
+            {/* @ts-ignore */}
+            <KambazNavigation />
+            <div className="wd-main-content-offset p-3">
+                <Routes>
+                    {/* @ts-ignore */}
+                    <Route path="/" element={<Navigate to="Account" />} />
+                    {/* @ts-ignore */}
+                    <Route path="/Account/*" element={<Account />} />
+                    {/* @ts-ignore */}
+                    <Route path="/Dashboard" element={
+                        <ProtectedRoute>
+                            <Dashboard
+                                //@ts-ignore
+                                courses={courses}
+                                course={course}
+                                setCourse={setCourse}
+                                addNewCourse={addNewCourse}
+                                deleteCourse={deleteCourse}
+                                updateCourse={updateCourse}
+                            />
+                        </ProtectedRoute>
+                    } />
+                    {/* @ts-ignore */}
+                    <Route path="/Courses/:courseId/*" element={
+                        <ProtectedRoute>
+                            <Courses courses={courses} />
+                        </ProtectedRoute>
+                    } />
+                    {/* @ts-ignore */}
+                    <Route path="/Calendar" element={<h1>Calendar</h1>} />
+                    {/* @ts-ignore */}
+                    <Route path="/Inbox" element={<h1>Inbox</h1>} />
+                </Routes>
+            </div>
         </div>
-    );}
+    );
+}
